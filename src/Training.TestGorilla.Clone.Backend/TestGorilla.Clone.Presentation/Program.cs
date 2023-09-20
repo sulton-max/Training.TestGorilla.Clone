c0f7c937bfeefd158b7aab717fbbd0fd25ba55fd
Jasurbek1223 <jasurabdulxaev@gmail.com>
2023-09-20 18:15:59 +0500

    Add new presentation layer

---
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Clone.Presentation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestGorilla.Clone.Presentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
