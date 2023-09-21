0a96ffd1beb006601c1a849c2dd6be8af71197e7
Azam Amonov <azamamonov555@gmail.com>
2023-09-21 18:09:13 +0500

    clear code

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Auditable/Auditable.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Auditable/Auditable.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestGorilla.Domain.Auditable
+﻿namespace TestGorilla.Domain.Auditable;
+public abstract class Auditable
 {
-    public abstract class Auditable
-    {
-    }
 }
