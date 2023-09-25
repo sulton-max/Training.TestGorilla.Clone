572b181054c4ae47ad1e43f4757968d66fc2b4e0
Bunyod <kbunyod3011@gmail.com>
2023-09-25 18:44:00 +0500

    Create Exam model

---
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Models/Exam.cs
@@ -0,0 +1,18 @@
+﻿namespace TestGorilla.Domain.Models;
+public class Exam : Auditable
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public TimeSpan Duration { get; set; }
+    public long CreatorId { get; set; }
+    public long ExaminatorId { get; set; }
+    public List<Test> Tests { get; set; }
+    public List<User> Candidates { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsDeleted { get; set; }
+
+    public Exam()
+    {
+
+    }
+}
