a6820b36fcf7fff427d916cc5f21526c4966fe77
Firdavs Asadov <toshmurodovj13@gmail.com>
2023-09-26 17:16:05 +0500

    Deleted namespace

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Models/Result.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Models/Result.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TestGorilla.Domain.Commons;
+﻿using TestGorilla.Domain.Commons;
 
 namespace TestGorilla.Domain.Models
 {
