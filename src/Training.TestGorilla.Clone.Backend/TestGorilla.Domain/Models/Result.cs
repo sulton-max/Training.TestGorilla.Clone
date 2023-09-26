83f82d1633e4a39d300ecc68e88b4f8faae0be01
Firdavs Asadov <toshmurodovj13@gmail.com>
2023-09-26 14:39:54 +0500

    Category va Result model is ready :)

---
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Models/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGorilla.Domain.Models
+{
+    public class Result : Auditable
+    {
+        public long UserId { get; set; }
+        public long TestId { get; set; }
+        public long ExamId { get; set; }
+        public long CategoryId { get; set; }
+        public decimal TestResult { get; set; }
+        public decimal ExamResult { get; set;}
+        public bool IsDelete { get; set; }
+        public Result()
+        {
+            
+        }
+    }
+}
