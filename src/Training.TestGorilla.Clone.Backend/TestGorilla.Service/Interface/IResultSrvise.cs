28326abe43bfb22765d792e2b29557f07eefd21f
MuhammadrizoVositov <vositovmuhammadrizo13@gmail.com>
2023-09-28 18:44:50 +0500

    CHanged Result Service

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/Interface/IResultSrvise.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/Interface/IResultSrvise.cs
@@ -1,25 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 using TestGorilla.Domain.Models;
 
-namespace TestGorilla.Service.Interface
+namespace TestGorilla.Service.Interface;
+
+public interface IResultSrvise
 {
-    public interface IResultSrvise
-    {
-        IQueryable<Result> Get(Expression<Func<Result, bool>> predicate);
+    IQueryable<Result> Get(Expression<Func<Result, bool>> predicate);
 
-        ValueTask<Result?> GetByIdAsync(Guid id);
+    ValueTask<Result?> GetByIdAsync(Guid id);
 
-        ValueTask<Result> CreateAsync(Result resut);
+    ValueTask<Result> CreateAsync(Result resut);
 
-        ValueTask<Result> UpdateAsync(Result Result);
+    ValueTask<Result> UpdateAsync(Result Result);
 
-        ValueTask<Result> DeleteAsync(Guid id);
+    ValueTask<Result> DeleteAsync(Guid id);
 
-        ValueTask<Result> DeleteAsync(Result Result);
-    }
+    ValueTask<Result> DeleteAsync(Result Result);
 }
