8a2a88a4622f50bed8cb142de200698d8376afcc
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-09-28 12:37:54 +0500

    keep on

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/Services/UserCredentialsService.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/Services/UserCredentialsService.cs
@@ -54,18 +54,42 @@ public class UserCredentialsService : IUserCredentialsService
         throw new NotImplementedException();
     }
 
-    private bool IsExistsUserCredentials(UserCredentials userCredentials)
+    private bool IsExistsUserCredentials(Guid id)
     {
-        return true;
+        if (_appDataContext.UserCredentials.Any(userCredential => userCredential.Id == id))
+            return true;
+        return false;
     }
 
     private bool IsValidToCreate(UserCredentials userCredentials)
     {
+        if (IsExistsUserCredentials(userCredentials.Id))
+            throw new InvalidOperationException("User Credentials already exists");
+        
+        if (_appDataContext.UserCredentials.Any(userCreadential => userCreadential.UserId == userCredentials.UserId))
+            throw new InvalidOperationException("The given user already has Creadentials");
+        
+        IsValidPassword(userCredentials.Password);
+
         return true;
     }
 
     private bool IsValidToUpdate(UserCredentials userCredentials)
     {
+        if(!IsExistsUserCredentials(userCredentials.Id))
+            throw new InvalidOperationException("User Credentials doesn't exists");
+
+        IsValidPassword(userCredentials.Password);
+
         return true;
+    }
+
+    private void IsValidPassword(string password)
+    {
+        Validator valid = new Validator();
+        if (valid.IsValidPassword(password))
+        {
+            throw new InvalidOperationException("Password is not available");
+        }
     }
 }
