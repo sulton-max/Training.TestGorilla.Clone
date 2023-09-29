d176707a567de74181bb70693b3fc8c11abef83a
Firdavs Asadov <toshmurodovj13@gmail.com>
2023-09-29 21:48:34 +0500

    Adding File set

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Questions/ShortAnswerTypeQuestion.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Questions/ShortAnswerTypeQuestion.cs
@@ -1,6 +1,11 @@
+using TestGorilla.Domain.Commons;
+
 namespace TestGorilla.Domain.Entities.Questions;
 
-public class ShortAnswerTypeQuestion
+public class ShortAnswerTypeQuestion : Auditable, IQuestion
 {
-    
+    public string Title { get; set ; }
+    public string Description { get ; set ; }
+    public TimeSpan Duration { get; set; }
+    public Category Category { get ; set ; }
 }
