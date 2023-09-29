efeee9dde64ef52a30c32c744de8afde25a47a66
Bunyod <kbunyod3011@gmail.com>
2023-09-29 22:02:01 +0500

    fixed Auditable and User model

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Commons/Auditable.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Commons/Auditable.cs
@@ -2,7 +2,7 @@ namespace TestGorilla.Domain.Commons;
 
 public abstract class Auditable
 {
-    public Guid Id { get; set;  }
+    public Guid Id { get; set; }
     public DateTime CreatedTime { get; set; }
     public DateTime UpdatedTime { get; set; }
 }
