ba45483d28f3622a64cad612331c6b808207ac39
Azam Amonov <azamamonov555@gmail.com>
2023-09-29 22:00:10 +0500

    Create UserAnswers model

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Users/UserAnswers.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Users/UserAnswers.cs
@@ -1,6 +1,11 @@
+using TestGorilla.Domain.Commons;
+using TestGorilla.Domain.Entities.Answers;
+
 namespace TestGorilla.Domain.Entities.Users;
 
-public class UserAnswers
+public class UserAnswers : Auditable
 {
-    
+    public Guid UserId { get; set; }
+    public Guid AssessmentId { get; set; }
+    public ICollection<Answer>? Answer { get; set; }
 }
