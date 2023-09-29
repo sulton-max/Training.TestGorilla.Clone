8871e37cba4ff455cd4fb864d6449066d7c86937
Muhammadaziz Komiljonov <azizbek1820@gmail.com>
2023-09-29 20:42:13 +0500

    Added  answer model

---
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Answers/Answer.cs
@@ -0,0 +1,7 @@
+﻿namespace TestGorilla.Domain.Entities.Answers;
+public class Answer
+{
+    public Guid Id {  get; set; }
+    public Guid QuestionId { get; set; }
+    public bool IsCorrect { get; set; }
+}
