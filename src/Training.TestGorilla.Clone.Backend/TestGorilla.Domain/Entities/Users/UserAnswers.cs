8b57ac616a981358a3880f1378cc01f88765dcdb
Azam Amonov <azamamonov555@gmail.com>
2023-09-29 22:26:16 +0500

    UserAnswers update

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Users/UserAnswers.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Users/UserAnswers.cs
@@ -7,8 +7,14 @@ namespace TestGorilla.Domain.Entities.Users;
 public class UserAnswers : Auditable
 {
     public Guid UserId { get; set; }
-    public Guid AssessmentId { get; set; }
-    public ICollection<IQuestion>? Questions { get; set; }
-    public ICollection<Answer>? Answer { get; set; }
+    public User? User { get; set; }
     
+    public Guid AssessmentId { get; set; }
+    public Assessment? Assessment { get; set; }
+    
+    public Guid QuestionsId { get; set; }
+    public IQuestion? Question { get; set; }
+    
+    public Guid? AnswerId { get; set; }
+    public Answer? Answer { get; set; }
 }
