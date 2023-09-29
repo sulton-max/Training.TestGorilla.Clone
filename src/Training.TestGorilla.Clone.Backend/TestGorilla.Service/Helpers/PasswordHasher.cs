1244c0dd9553a0dc93601c05710e21933e88f8f8
Azam Amonov <azamamonov555@gmail.com>
2023-09-29 22:22:08 +0500

    Password hasher created

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/Helpers/PasswordHasher.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/Helpers/PasswordHasher.cs
@@ -4,7 +4,7 @@ public static class PasswordHasher
 {
     public static string Hash(this string password) =>
                     BCrypt.Net.BCrypt.HashPassword(password);
-        
+    
     public static bool Verify(this string password, string hashedPassword) =>
                     BCrypt.Net.BCrypt.Verify(password, hashedPassword);
 }
