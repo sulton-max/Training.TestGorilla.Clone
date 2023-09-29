a85afefffb0c30415506b1aa2ddf36d2732536b8
Azam Amonov <azamamonov555@gmail.com>
2023-09-29 22:01:48 +0500

    Add IQuestion Collection in UserAnswers

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Users/UserAnswers.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Users/UserAnswers.cs
@@ -1,5 +1,6 @@
 using TestGorilla.Domain.Commons;
 using TestGorilla.Domain.Entities.Answers;
+using TestGorilla.Domain.Entities.Questions;
 
 namespace TestGorilla.Domain.Entities.Users;
 
@@ -7,5 +8,6 @@ public class UserAnswers : Auditable
 {
     public Guid UserId { get; set; }
     public Guid AssessmentId { get; set; }
+    public ICollection<IQuestion>? Questions { get; set; }
     public ICollection<Answer>? Answer { get; set; }
 }
