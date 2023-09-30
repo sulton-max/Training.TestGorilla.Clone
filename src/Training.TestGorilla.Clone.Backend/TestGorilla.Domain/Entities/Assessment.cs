372f6c2d4b4deba41e7ad0e4149d371175f7cdd5
Azam Amonov <azamamonov555@gmail.com>
2023-09-30 23:01:17 +0500

    Create properties for Assessment.cs

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Assessment.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Assessment.cs
@@ -1,9 +1,32 @@
+using TestGorilla.Domain.Commons;
+using TestGorilla.Domain.Entities.Users;
+
 namespace TestGorilla.Domain.Entities;
 
 /// <summary>
 /// Assessment is as a exam class
 /// </summary>
-public class Assessment
+public class Assessment : Auditable, IEntity
 {
+    public ICollection<User> Users { get; set; }
+
+    public ICollection<Test> Tests { get; set; }
     
+    public TimeSpan Duration { get; set; }
+    
+    public bool IsActive { get; set; }
+    
+    public bool IsDeleted { get; set; }
+
+    public Assessment()
+    {
+    }
+    public Assessment(ICollection<User> users, ICollection<Test> tests, TimeSpan duration)
+    {
+        Users = users;
+        Tests = tests;
+        Duration = duration;
+        IsActive = false;
+        IsDeleted = false;
+    }
 }
