cef24c4d9f197e9f70a8bb34149856d3c74bfdfd
Muhammadaziz Komiljonov <azizbek1820@gmail.com>
2023-09-30 17:02:06 +0500

    Added IAnswerService iterface

---
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/Interface/IAnswerService.cs
@@ -0,0 +1,13 @@
+﻿using TestGorilla.Domain.Entities.Answers;
+using TestGorilla.Domain.Entities.Users;
+
+namespace TestGorilla.Service.Interface;
+
+public interface IAnswerService
+{
+    IQueryable<Answer> Get();
+    ValueTask<Answer> GetByIdAsync(Guid id);
+    ValueTask<Answer> CreateAsync(Answer answer);
+    ValueTask<Answer> UpdateAsync(Answer answer);
+    ValueTask<Answer> DeleteAsync(Guid answeId);
+}
