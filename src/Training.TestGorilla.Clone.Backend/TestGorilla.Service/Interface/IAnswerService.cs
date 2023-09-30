111e00111c98a69e7780a95c37336fdbe94c6d9e
Muhammadaziz Komiljonov <azizbek1820@gmail.com>
2023-09-30 18:28:21 +0500

    Edited one of the method's dependences of IAnswersService interface

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/Interface/IAnswerService.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/Interface/IAnswerService.cs
@@ -1,13 +1,15 @@
-﻿using TestGorilla.Domain.Entities.Answers;
+﻿using System.Linq.Expressions;
+using TestGorilla.Domain.Entities.Answers;
 using TestGorilla.Domain.Entities.Users;
 
 namespace TestGorilla.Service.Interface;
 
 public interface IAnswerService
 {
-    IQueryable<Answer> Get();
+    IQueryable<Answer> Get(Expression<Func<Answer, bool>> predicate);
     ValueTask<Answer> GetByIdAsync(Guid id);
     ValueTask<Answer> CreateAsync(Answer answer);
     ValueTask<Answer> UpdateAsync(Answer answer);
     ValueTask<Answer> DeleteAsync(Guid answeId);
+    ValueTask<ICollection<Answer>> GetByQuestionIdAsync(Guid questionId);
 }
