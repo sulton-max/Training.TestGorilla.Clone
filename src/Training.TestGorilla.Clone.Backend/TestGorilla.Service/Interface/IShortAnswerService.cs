81bc3503a7ca2f92712020289e1daf4c0df6a816
Azam Amonov <azamamonov555@gmail.com>
2023-09-30 23:44:01 +0500

    Bugs fixed on Answer model and service

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/Interface/IShortAnswerService.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/Interface/IShortAnswerService.cs
@@ -4,10 +4,10 @@ using TestGorilla.Domain.Entities.Answers;
 namespace TestGorilla.Service.Interface;
 
 public interface IShortAnswerService
-{
+{ 
     IQueryable<ShortAnswer> Get(Expression<Func<ShortAnswer, bool>> predicate);
     ValueTask<ShortAnswer> GetByIdAsync(Guid id);
     ValueTask<ShortAnswer> CreateAsync(ShortAnswer answer, bool saveChanges = true, CancellationToken cancellationToken = default);
     ValueTask<ShortAnswer> UpdateAsync(ShortAnswer answer, bool saveChanges = true, CancellationToken cancellationToken = default);
-    ValueTask<ShortAnswer> DeleteAsync(Guid answeId, bool saveChanges = true, CancellationToken cancellationToken = default);
+    ValueTask<ShortAnswer> DeleteAsync(Guid answerId, bool saveChanges = true, CancellationToken cancellationToken = default);
 }
