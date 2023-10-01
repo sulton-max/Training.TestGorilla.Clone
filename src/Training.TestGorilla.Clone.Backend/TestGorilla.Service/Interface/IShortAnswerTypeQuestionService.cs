d67e0ae780b6a323fe458d11c7bd0e52c80d40fa
Firdavs Asadov <toshmurodovj13@gmail.com>
2023-10-01 17:29:43 +0500

    Adding interfeys ShortAnswerTypeQuestion service

---
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/Interface/IShortAnswerTypeQuestionService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TestGorilla.Domain.Entities;
+using TestGorilla.Domain.Entities.Questions;
+
+namespace TestGorilla.Service.Interface
+{
+    public interface IShortAnswerTypeQuestionService
+    {
+        public Task<ShortAnswerTypeQuestion> CreateAsync(ShortAnswerTypeQuestion question, CancellationToken cancellationToken, bool saveChanges = true);
+    
+        public Task<ShortAnswerTypeQuestion> UpdateAsync(ShortAnswerTypeQuestion question, CancellationToken cancellationToken, bool saveChanges = true);
+    
+        public Task<bool> DeleteAsync(Guid questionId, CancellationToken cancellationToken, bool saveChanges = true);
+    
+        public IQueryable<ShortAnswerTypeQuestion> Get(Expression<Func<ShortAnswerTypeQuestion, bool>> predicate, CancellationToken cancellationToken, bool saveChanges = true);
+
+        public Task<PaginationResult<ShortAnswerTypeQuestion>> GetAsync(ShortAnswerTypeQuestion question, int PageToken, int PageSize,
+            CancellationToken cancellationToken, bool saveChanges = true);
+    
+        public Task<ShortAnswerTypeQuestion> GetByIdAsync(Guid id, CancellationToken cancellationToken, bool saveChanges = true);
+    
+        public Task<IEnumerable<ShortAnswerTypeQuestion>> GetByTitleAsync(string Title, CancellationToken cancellationToken, bool saveChanges = true);
+    
+        public Task<IEnumerable<ShortAnswerTypeQuestion>> GetByCategoryAsync(Category category, CancellationToken cancellationToken, bool saveChanges = true);
+    }
+}
