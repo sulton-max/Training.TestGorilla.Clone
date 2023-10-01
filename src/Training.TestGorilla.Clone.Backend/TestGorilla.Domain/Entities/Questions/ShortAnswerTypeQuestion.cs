0e542a6279597ca24aea4183162ebb3123c167d0
Firdavs Asadov <toshmurodovj13@gmail.com>
2023-10-01 17:26:43 +0500

    Short answer adding consturctor

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Questions/ShortAnswerTypeQuestion.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Questions/ShortAnswerTypeQuestion.cs
@@ -8,4 +8,12 @@ public class ShortAnswerTypeQuestion : Auditable, IQuestion
     public string Description { get ; set ; }
     public TimeSpan Duration { get; set; }
     public Category Category { get ; set ; }
+
+    public ShortAnswerTypeQuestion(string title, string description, TimeSpan duration, Category category)
+    {
+        Title = title;
+        Description = description;
+        Duration = duration;
+        Category = category;
+    }
 }
