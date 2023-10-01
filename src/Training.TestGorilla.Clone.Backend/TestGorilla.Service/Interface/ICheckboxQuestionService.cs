ca321fa79f25134c7bde84478caed779ac37997f
Firdavs Asadov <toshmurodovj13@gmail.com>
2023-10-01 14:08:06 +0500

    adding Check box question and pagination class

---
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/Interface/ICheckboxQuestionService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TestGorilla.Domain.Entities;
+using TestGorilla.Domain.Entities.Questions;
+
+namespace TestGorilla.Service.Interface
+{
+    public interface ICheckboxQuestionService
+    {
+        public Task<CheckBoxQuestion> CreateAsync(CheckBoxQuestion question, CancellationToken cancellationToken, bool saveChanges = true);
+        public Task<CheckBoxQuestion> UpdateAsync(CheckBoxQuestion question, CancellationToken cancellationToken, bool saveChanges = true);
+        public Task<bool> DeleteAsync(Guid questionId, CancellationToken cancellationToken, bool saveChanges = true);
+        public IQueryable<CheckBoxQuestion> Get(Expression<Func<CheckBoxQuestion, bool>> predicate, CancellationToken cancellationToken, bool saveChanges = true);
+
+        public Task<PaginationResult<CheckBoxQuestion>> GetAsync(CheckBoxQuestion question, int PageToken, int PageSize,
+            CancellationToken cancellationToken, bool saveChanges = true);
+        public Task<CheckBoxQuestion> GetByIdAsync(Guid id, CancellationToken cancellationToken, bool saveChanges = true);
+        public Task<IEnumerable<CheckBoxQuestion>> GetByTitleAsync(string Title, CancellationToken cancellationToken, bool saveChanges = true);
+        public Task<IEnumerable<CheckBoxQuestion>> GetByCategoryAsync(Category category, CancellationToken cancellationToken, bool saveChanges = true);
+    }
+}
