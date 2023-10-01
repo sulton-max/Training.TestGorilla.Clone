b196ec8041404f9f64bb709d703417bd44fe49e7
MuhammadrizoVositov <vositovmuhammadrizo13@gmail.com>
2023-10-01 14:16:24 +0500

    Create result service

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Result.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Result.cs
@@ -1,8 +1,35 @@
+using TestGorilla.Domain.Commons;
+
 namespace TestGorilla.Domain.Entities;
 /// <summary>
 /// Result calculate userAnswers from tests
 /// </summary>
-public class Result
+public class Result : Auditable
 {
+
+    public Guid UserId { get; set; }
+
+    public Guid TestId { get; set; }
+
+    public Guid ExamId { get; set; }
+
+    public Guid CategoryId { get; set; }
+
+    public decimal TestResult { get; set; }
+
+    public decimal ExamResult { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public Result(Guid userId, Guid testId, Guid examId, Guid categoryId, decimal testResult, decimal examResult, bool isDeleted)
+    {
+        UserId = userId;
+        TestId = testId;
+        ExamId = examId;
+        CategoryId = categoryId;
+        TestResult = testResult;
+        ExamResult = examResult;
+        IsDeleted = isDeleted;
+    }
     
 }
