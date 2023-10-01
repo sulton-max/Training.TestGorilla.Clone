1e95bd746a1d4c650802b1f953e22bd3aaaba709
A'zam Amonov <84405537+Amonov996@users.noreply.github.com>
2023-10-01 09:51:23 +0000

    Update PaginationResult.cs

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/PaginationResult.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/PaginationResult.cs
@@ -3,7 +3,10 @@
 public class PaginationResult<T>
 {
     public IEnumerable<T> Items { get; set; }
+    
     public int TotalItems { get; set; }
+    
     public int PageToken { get; set; }
+    
     public int PageSize { get; set; }
 }
