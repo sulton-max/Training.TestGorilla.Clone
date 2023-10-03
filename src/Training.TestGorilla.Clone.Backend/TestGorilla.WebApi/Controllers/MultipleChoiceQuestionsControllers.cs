30f53524c4e59c66ec981b198a611210d7ab7987
Firdavs Asadov <toshmurodovj13@gmail.com>
2023-10-03 16:39:46 +0500

    adding Question Dtos folder

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.WebApi/Controllers/MultipleChoiceQuestionsControllers.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.WebApi/Controllers/MultipleChoiceQuestionsControllers.cs
@@ -1,6 +1,18 @@
-﻿namespace TestGorilla.Api.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using TestGorilla.Service.Interface;
+
+namespace TestGorilla.Api.Controllers
 {
-    public class MultipleChoiceQuestionsControllers
+    [ApiController]
+    [Route("api/controller")]
+    public class MultipleChoiceQuestionsControllers : ControllerBase
     {
+        private readonly IMultipleChoiceQuestionService _multipleChoiceQuestionService;
+        public MultipleChoiceQuestionsControllers(IMultipleChoiceQuestionService multipleChoiceQuestionService)
+        {
+            _multipleChoiceQuestionService = multipleChoiceQuestionService;
+        }
+        [HttpGet("questionId:guid")]
+
     }
 }
