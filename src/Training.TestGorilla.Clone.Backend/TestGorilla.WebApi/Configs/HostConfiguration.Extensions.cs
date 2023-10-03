7a82c4c07454ae08d5a5cea4ad7a96a9cbf3e58f
Firdavs Asadov <toshmurodovj13@gmail.com>
2023-10-03 09:37:04 +0500

    Adding configurations

---
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.WebApi/Configs/HostConfiguration.Extensions.cs
@@ -0,0 +1,31 @@
+﻿using FileBaseContext.Abstractions.Models.FileContext;
+using FileBaseContext.Context.Models.Configurations;
+using Microsoft.Extensions.DependencyInjection;
+using TestGorilla.DataAccess.Context;
+using TestGorilla.Service.Interface;
+using TestGorilla.Service.Service;
+
+namespace TestGorilla.Api.Configs
+{
+    public static class HostConfiguration
+    {
+        public static WebApplicationBuilder AddDataContext(this WebApplicationBuilder builder)
+        {
+            var fileContextOptions = new FileContextOptions<AppFileContext>(Path.Combine(builder.Environment.ContentRootPath, "Data/Storage"));
+
+            builder.Services.AddSingleton(fileContextOptions); // Remove generic type parameter
+            builder.Services.AddScoped<IDataContext, AppFileContext>(provider =>
+            {
+                var options = provider.GetRequiredService<IFileContextOptions<AppFileContext>>(); // Change this line
+                var dataContext = new AppFileContext(options);
+                dataContext.FetchAsync().AsTask().Wait();
+
+                return dataContext;
+            });
+            return builder;
+        }
+        
+        
+
+    }
+}
---
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.WebApi/Configs/HostConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace TestGorilla.Api.Configs
+{
+    public class HostConfiguration
+    {
+        public static WebApplicationBuilder Configure(this WebApplicationBuilder builder)
+        {
+            builder.AddDataContext();
+            builder.Services();
+            builder.AddDevTools();
+            return builder;
+        }
+        public static async ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+        {
+            app.UseDevTools();
+        }
+    }
+}
