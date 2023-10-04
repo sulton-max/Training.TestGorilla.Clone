6cf0ebd6a11b6c40cef351fac9112ab97c82a31f
Firdavs Asadov <toshmurodovj13@gmail.com>
2023-10-04 17:09:23 +0500

    add constructor with services

---
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.WebApi/Controllers/QuestionsController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TestGorilla.Service.Interface;
+
+namespace TestGorilla.Api.Controllers
+{
+    [ApiController]
+    [Route("api/controller")]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly IMultipleChoiceQuestionService _multipleChoiceQuestionService;
+        private readonly ICheckboxQuestionService _checkboxQuestionService;
+        private readonly IShortAnswerTypeQuestionService _shortAnswerTypeQuestionService;
+        private readonly IMapper _mapper;
+        public QuestionsController(IMultipleChoiceQuestionService multipleChoiceQuestionService
+            ,ICheckboxQuestionService checkboxQuestionService, IShortAnswerTypeQuestionService shortAnswerTypeQuestionService,
+            IMapper mapper)
+        {
+            _multipleChoiceQuestionService = multipleChoiceQuestionService;
+            _checkboxQuestionService = checkboxQuestionService;
+            _shortAnswerTypeQuestionService = shortAnswerTypeQuestionService;
+            _mapper = mapper;
+        }
+    }
+}
