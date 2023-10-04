4d4faf5cbe015df397c5f6bb7cd2ee13f57e6af1
Firdavs Asadov <toshmurodovj13@gmail.com>
2023-10-04 17:03:23 +0500

    adding all questions dtos class

---
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Service/DTOs/Questions/MultipleChoiceDTOs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGorilla.Service.DTOs.Questions
+{
+    public class MultipleChoiceDTOs
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TimeSpan Duration { get; set; }
+        public MultipleChoiceDTOs()
+        {
+            
+        }
+    }
+}
