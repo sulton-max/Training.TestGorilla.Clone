59f59ee72aa6cdd8c753e9819758b1feb914fc9f
Bunyod <kbunyod3011@gmail.com>
2023-10-04 12:09:55 +0500

    #134  Fix Get() method in userService(return with pagination) and add  validation in Delete() method

---
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/PaginationResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGorilla.Domain.Entities
+{
+    public class PaginationResult<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int TotalItems { get; set; }
+        public int PageToken { get; set; }
+        public int PageSize { get; set; }
+    }
+}
