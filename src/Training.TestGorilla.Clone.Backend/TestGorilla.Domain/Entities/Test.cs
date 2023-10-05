1e254dfd02067b2d5a944e328bef0e82624b9cda
Jasurbek1223 <jasurabdulxaev@gmail.com>
2023-10-05 14:43:22 +0500

    fexed Test Model

--- src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Test.cs
+++ src/Training.TestGorilla.Clone.Backend/TestGorilla.Domain/Entities/Test.cs
@@ -12,20 +12,4 @@ public class Test : Auditable
     public QuestionLevel QuestionLevel { get; set; }
 
     public int Duration { get; set; }
-
-   /* public Test(Guid id,string title, string description, QuestionLevel questionLevel, DateTime createdTime, DateTime updatedTime, TimeSpan duration)
-    {
-        Id = id;    
-        Title = title;
-        Description = description;
-        QuestionLevel = questionLevel;
-        CreatedTime = createdTime;
-        UpdatedTime = default(DateTime);
-        Duration = duration;
-    }*/
-
-    public override string ToString()
-    {
-        return $"Title : {Title}, Description : {Description}, Question Level : {QuestionLevel}, Duration : {Duration}";
-    }
 }
